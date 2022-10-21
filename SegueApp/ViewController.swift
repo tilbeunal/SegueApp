7cb86f9a8827de878e9ddb7621a9846ef892735b
tilbeunalsengezer <tilbeunal@gmail.com>
2022-10-22 02:50:54 +0300

    Initial Commit

---
+++ SegueApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by Tilbe Ünal on 22.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
